c1cac8d83a65b2e51839d1729c64f98b0f771fbd
Mrcruz23 <mrcharlescruz23@gmail.com>
2026-07-30 22:24:04 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Bump this version string every time index.html changes so old cached
 // copies get thrown away instead of being served forever.
-const CACHE_NAME = 'bosque-rojo-v4';
+const CACHE_NAME = 'bosque-rojo-v5';
 const ASSETS = [
   './',
   './index.html',
